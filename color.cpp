4e747f2306c2a4d35bdbc444f9aa170f08e6a002
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-04-04 11:49:08 -0700

    gamma correction

--- color.cpp
+++ color.cpp
@@ -1,6 +1,13 @@
 #include "color.h"
 
 #include "interval.h"
+#include "math.h"
+
+namespace {
+	inline double linear_to_gamma(double linear) {
+		return sqrt(linear);
+	}
+}
 
 void write_color_256(std::ostream& out, Color pixel_color, int samples_per_pixel)
 {
@@ -12,6 +19,10 @@ void write_color_256(std::ostream& out, Color pixel_color, int samples_per_pixel
 	r *= scale;
 	g *= scale;
 	b *= scale;
+
+	r = linear_to_gamma(r);
+	g = linear_to_gamma(g);
+	b = linear_to_gamma(b);
 
 	static const Interval intensity(0.000, 0.999);
 	static const int max_value = 256;
