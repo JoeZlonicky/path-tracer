34b561ae61b102c9e9e8b5fc7e3c73605ebcd0e2
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-04-04 15:54:41 -0700

    metal fuzz

--- metal.cpp
+++ metal.cpp
@@ -1,15 +1,18 @@
 #include "metal.h"
 
 #include "hittable.h"
+#include "utility.h"
 
-Metal::Metal(const Color& albedo) : _albedo(albedo)
+Metal::Metal(const Color& albedo, double fuzz) : _albedo(albedo), _fuzz(fuzz <= 1.0 ? fuzz : 1.0)
 {
 }
 
 bool Metal::scatter(const Ray& r, const HitRecord& record, Color& attenuation_out, Ray& scattered_out) const
 {
     Vector3 reflected = r.getDirection().normalized().reflect(record.normal);
-    scattered_out = Ray{ record.p, reflected };
+    scattered_out = Ray{ record.p, reflected + _fuzz * random_unit_vector()};
     attenuation_out = _albedo;
-    return true;
+
+    auto did_fuzz_place_below_surface = scattered_out.getDirection().dot(record.normal) <= 0;
+    return !did_fuzz_place_below_surface;
 }
--- metal.h
+++ metal.h
@@ -4,10 +4,11 @@
 
 class Metal : public Material {
 public:
-	Metal(const Color& albedo);
+	Metal(const Color& albedo, double fuzz);
 
 	bool scatter(const Ray& r, const HitRecord& record, Color& attenuation_out, Ray& scattered_out) const override;
 	
 private:
 	Color _albedo;
+	double _fuzz;
 };
