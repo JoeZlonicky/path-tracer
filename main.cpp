442c8b9ebc2cb983cc1310f8def71b5525caefaf
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-04-04 21:54:31 -0700

    final render setup

--- main.cpp
+++ main.cpp
@@ -16,33 +16,64 @@ static void create_sphere(HittableList& world, double x, double y, double z, dou
 }
 
 int main() {
-	// Materials
-	auto ground_mat = std::make_shared<Lambertian>(Color{ 0.8, 0.8, 0.0 });
-	auto center_mat = std::make_shared<Lambertian>(Color{ 0.0, 0.8, 0.8 });
-	auto left_mat = std::make_shared<Dielectric>(1.5);
-	auto right_mat = std::make_shared<Metal>(Color{ 0.8, 0.6, 0.2 }, 0.2);
-
-
 	// World
 	HittableList world;
-	create_sphere(world, 0.0, -100.5, -1, 100.0, ground_mat);
-	create_sphere(world, -1.0, 0.0, -1.0, 0.5, center_mat);
-	create_sphere(world, 0.0, 0.0, -1.0, 0.5, left_mat);
-	create_sphere(world, 1.0, 0.0, -1.0, 0.5, right_mat);
+	auto ground_mat = std::make_shared<Lambertian>(Color{ 0.5, 0.5, 0.5 });
+	create_sphere(world, 0.0, -1000.0, -0.0, 1000.0, ground_mat);
+
+	for (int a = -11; a < 11; ++a) {
+		for (int b = -11; b < 11; ++b) {
+			auto choose_mat = random_normalized();
+			Point3 center{ a + 0.9 * random_normalized(), 0.2, b + 0.9 * random_normalized() };
+
+			if ((center - Vector3{ 4.0, 0.2, 0.0 }).magnitude() > 0.9) {
+				std::shared_ptr<Material> sphere_material;
+
+				if (choose_mat < 0.8) {
+					// Diffuse
+					auto albedo = Color::random() * Color::random();
+					sphere_material = std::make_shared<Lambertian>(albedo);
+					world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
+				}
+				else if (choose_mat < 0.95) {
+					// Metal
+					auto albedo = Color::random(0.5, 1.0);
+					auto fuzz = random_range(0.0, 0.5);
+					sphere_material = std::make_shared<Metal>(albedo, fuzz);
+					world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
+				}
+				else {
+					// Glass
+					sphere_material = std::make_shared<Dielectric>(1.5);
+					world.add(std::make_shared<Sphere>(center, 0.2, sphere_material));
+				}
+			}
+		}
+	}
+
+	auto mat_1 = std::make_shared<Dielectric>(1.5);
+	create_sphere(world, 0.0, 1.0, 0.0, 1.0, mat_1);
+
+	auto mat_2 = std::make_shared<Lambertian>(Color{ 0.4, 0.2, 0.1 });
+	create_sphere(world, -4.0, 1.0, 0.0, 1.0, mat_2);
+
+	auto mat_3 = std::make_shared<Metal>(Color{ 0.7, 0.6, 0.5 }, 0.0);
+	create_sphere(world, 4.0, 1.0, -1.0, 1.0, mat_3);
 
 	// Camera
 	Camera cam;
 	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 400;
-	cam.max_bounces = 50;
+	cam.image_width = 300;
+	cam.max_bounces = 20;
 	cam.samples_per_pixel = 10;
+
 	cam.vfov = 20.0;
-	cam.pos = { -2, 2, 1 };
-	cam.look_at = { 0.0, 0.0, -1.0 };
+	cam.pos = { 13.0, 2.0, 3.0 };
+	cam.look_at = { 0.0, 0.0, 0.0 };
 	cam.up = { 0.0, 1.0, 0.0 };
 
-	cam.defocus_angle = 10.0;
-	cam.focus_distance = 3.4;
+	cam.defocus_angle = 0.6;
+	cam.focus_distance = 10.0;
 
 	std::ofstream output;
 	output.open("output.ppm");
