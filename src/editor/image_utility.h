a8b636c36ec745e884581f116d5f6ccf86811b8f
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-04-22 10:40:04 -0700

    unique save names

--- src/editor/image_utility.h
+++ src/editor/image_utility.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <cmath>
+#include <ctime>
 #include <SDL_image.h>
+#include <string>
 
 namespace ImageUtility {
 	// https://stackoverflow.com/questions/20070155/how-to-set-a-pixel-in-a-sdl-surface
@@ -17,7 +19,13 @@ namespace ImageUtility {
 		return 0.f;
 	}
 
-	inline void save_sdl_surface_as_png(SDL_Surface* surface) {
-		IMG_SavePNG(surface, "output.png");
+	inline std::string save_sdl_surface_as_png(SDL_Surface* surface) {
+		static int n = 1;
+
+		auto t = std::time(0);
+		auto filename = "output_" + std::to_string(t) + "_" + std::to_string(n++) + ".png";
+
+		IMG_SavePNG(surface, filename.c_str());
+		return filename;
 	}
 }
